5547dae2b95e512ae0d4a1e6026b593b21cf272f
Josh Justice <me@codingitwrong.com>
2018-10-23 20:06:56 -0400

    Add title and nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,18 @@
 module.exports = {
+  title: '@reststate/client',
   themeConfig: {
-    displayAllHeaders: true,
+    nav: [
+      { text: '@reststate/client', link: '/' },
+      { text: '/vuex', link: 'https://vuex.reststate.org' },
+      { text: '/mobx', link: 'https://mobx.reststate.org' },
+      { text: 'home', link: 'https://reststate.org' },
+    ],
     sidebar: [
       '/',
       'Installation',
       'Reading Data',
       'Writing Data',
     ],
+    displayAllHeaders: true,
   },
 };
